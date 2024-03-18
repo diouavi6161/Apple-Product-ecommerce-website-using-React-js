f7a7f675f871b9b3db867d941d1abd60bd19ca8c
avi <avilinid035@gmail.com>
2024-03-18 13:14:43 +0530

    upload files

---
+++ src/Data.jsx
@@ -0,0 +1,53 @@
+const DATA = [
+  {
+    id: 0,
+    title: "IPhone 10",
+    price: 250,
+    desc: "The iPhone X used a glass and stainless-steel form factor and bezel-less design, shrinking the bezels while not having a chin, unlike many Android phones. It was the first iPhone to use an OLED screen, branded as a Super Retina HD display. The home button's fingerprint sensor was replaced with a new type of authentication called Face ID, which uses sensors to scan the user's face to unlock the device. This face-recognition capability also enabled emojis to be animated following the user's expression (Animoji). With a bezel-less design, iPhone user interaction changed significantly, using gestures to navigate the operating system rather than the home button used in all previous iPhones. At the time of its November 2017 launch, its price tag of US$999 also made it the most expensive iPhone ever, with even higher prices internationally due to additional local sales and import taxes.",
+    img: "/assets/images/products/iphoneX.png",
+  },
+  {
+    id: 1,
+    title: "IPhone 11 Pro Max",
+    price: 300,
+    desc: "The iPhone 11 Pro features a sleek and durable design with a surgical-grade stainless steel frame and a matte glass back. It comes in a range of stylish colors, including Space Gray, Silver, Midnight Green, and Gold.It sports a 5.8-inch Super Retina XDR display with OLED technology, offering stunning brightness, true blacks, and high contrast ratios. The display supports HDR content and has a resolution of 2436 x 1125 pixels.Powered by the A13 Bionic chip, the iPhone 11 Pro delivers exceptional performance, enabling smooth multitasking, immersive gaming experiences, and efficient power management.It boasts improved battery life compared to its predecessors, offering up to 18 hours of video playback on a single charge. It also supports fast charging and wireless charging.",
+    img: "/assets/images/products/iphone11promax.png",
+  },
+  {
+    id: 2,
+    title: "IPhone 12 Mini",
+    price: 450,
+    desc: "The iPhone 12 Mini is a compact yet powerful smartphone designed and manufactured by Apple Inc. Launched in October 2020, it offers a smaller form factor compared to other models in the iPhone 12 lineup while retaining many of the flagship features.The iPhone 12 Mini features a smaller form factor with a 5.4-inch Super Retina XDR display, making it ideal for users who prefer a more compact device that fits comfortably in hand and pocket.Powered by the A14 Bionic chip, the iPhone 12 Mini delivers exceptional performance, whether you're multitasking, gaming, or using demanding apps. It offers smooth and responsive performance for a seamless user experience.With support for 5G networks, the iPhone 12 Mini enables faster download and upload speeds, as well as improved streaming and gaming experiences, in areas with 5G coverageThe iPhone 12 Mini features a dual-camera system consisting of a 12-megapixel Ultra Wide and Wide cameras. It allows users to capture stunning photos and videos, including impressive low-light performance and advanced computational photography features like Night mode.. ",
+    img: "/assets/images/products/iphone12mini.png",
+  },
+  {
+    id: 3,
+    title: "IPhone 12",
+    price: 450,
+    desc: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+    img: "/assets/images/products/iphone12.png",
+  },
+  {
+    id: 4,
+    title: "IPhone 12 Pro",
+    price: 500,
+    desc: "The iPhone 12 has a 6.1-inch OLED display with a resolution of 2532 x 1170 pixels.Powered by Apple's A14 Bionic chip, the iPhone 12 delivers exceptional performance and efficiency. This chip enables smooth multitasking, gaming, and other intensive tasks. One of the highlighted features of the iPhone 12 is its support for 5G networks, enabling faster download and upload speeds, improved streaming quality, and better overall connectivity.The iPhone 12 has a dual-camera system consisting of a 12-megapixel wide and ultra-wide lens. It offers features like Night mode, Deep Fusion, and Smart HDR 3, providing users with high-quality photos and videos in various lighting conditions.Apple introduced MagSafe technology with the iPhone 12 lineup, allowing for easy attachment of MagSafe accessories such as cases, chargers, and wallets magnetically to the back of the phone.",
+    img: "/assets/images/products/iphone12pro.png",
+  },
+  {
+    id: 5,
+    title: "IPhone 12 Pro Max",
+    price: 675,
+    desc: "The iPhone 12 Pro Max is a flagship smartphone produced by Apple Inc., released in November 2020.The iPhone 12 Pro Max features a premium design with a flat-edge stainless steel frame and a glass front and back. It comes in various colors including Graphite, Silver, Gold, and Pacific Blue.It boasts a large 6.7-inch Super Retina XDR OLED display with a resolution of 2778 x 1284 pixels. This display offers HDR support, True Tone technology, and a high contrast ratio for vibrant colors and deep blacks.Powered by Apple's A14 Bionic chip, the iPhone 12 Pro Max delivers excellent performance and efficiency. It's capable of handling demanding tasks, gaming, and multitasking with ease.The iPhone 12 Pro Max sports a triple-camera system consisting of a 12-megapixel Ultra Wide, Wide, and Telephoto lens. It offers features such as Night mode, Deep Fusion, Smart HDR 3, and 4K Dolby Vision HDR recording. The LiDAR (Light Detection and Ranging) sensor enhances augmented reality (AR) experiences and autofocus in low-light situations.",
+    img: "/assets/images/products/iphone12promax.png",
+  },
+  {
+    id: 6,
+    title: "IPhone 13 mini ",
+    price: 800,
+    desc: "The iPhone 13 Mini is a compact smartphone model produced by Apple Inc., released in September 2021.The iPhone 13 Mini features a sleek and compact design, offering a smaller form factor compared to other models in the iPhone 13 series.It sports a premium build with glass on the front and back, surrounded by an aluminum frame.Available color options typically include a range of finishes such as Starlight, Midnight, Blue, Pink, and (Product) RedThe device boasts a Super Retina XDR OLED display, offering vibrant colors, deep blacks, and high contrast.The iPhone 13 Mini comes equipped with a dual-camera system, including a wide and ultra-wide lens.It offers features like Night mode, Deep Fusion, and Smart HDR 4 for enhanced photo and video quality.Despite its compact size, the iPhone 13 Mini offers decent battery life, lasting through a full day of moderate usage.It supports fast charging and wireless charging capabilities for added convenience.",
+    img: "/assets/images/products/iphone13mini.png",
+  },
+];
+
+export default DATA;
